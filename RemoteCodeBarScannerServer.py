f6187dacc6f49afcd7b386c8801abbd266dda43d
Christophe Dugas du Villard <ch.dugasduvillard@gmail.com>
2018-05-31 14:34:51 +0200

    Add files via upload

---
+++ RemoteCodeBarScannerServer.py
@@ -0,0 +1,84 @@
+from http.server import BaseHTTPRequestHandler
+from urllib import parse
+from os import curdir, path, sep
+#from io
+
+class RCBSRequestHandler(BaseHTTPRequestHandler):
+
+    def do_GetPublic(self,html_path):
+        file_path = curdir+sep+html_path
+
+        # set mime type for return
+        if file_path.endswith(".html"):
+            mimetype='text/html'
+        if file_path.endswith(".jpg"):
+            mimetype='image/jpg'
+        if file_path.endswith(".gif"):
+            mimetype='image/gif'
+        if file_path.endswith(".js"):
+            mimetype='application/javascript'
+        if file_path.endswith(".css"):
+            mimetype='text/css'
+
+        #Open the static file requested and send it
+        if path.exists(file_path):
+            f = open(file_path, 'rb') 
+            self.send_response(200)
+            self.send_header('Content-type',mimetype)
+            self.end_headers()
+            self.wfile.write(f.read())
+            f.close()
+            return 
+        # send error 404 if file not found
+        self.send_error(404,'File Not Found: %s' % html_path)
+
+
+    def do_GET(self):
+        print('do_Get: '+self.path)
+        parsed_path = parse.urlparse(self.path)
+        splited_path = parsed_path.path.rsplit('/')
+        if len(splited_path) >= 1:
+            if(splited_path[1] == 'public'): # share only public files
+                self.do_GetPublic(parsed_path.path)
+            else:
+                self.do_GetPublic("public/index.html")
+        else:
+            self.do_GetPublic("public/index.html")
+
+'''
+        message_parts = [
+            'CLIENT VALUES:',
+            'client_address={} ({})'.format(
+                self.client_address,
+                self.address_string()),
+            'command={}'.format(self.command),
+            'path={}'.format(self.path),
+            'real path={}'.format(parsed_path.path),
+            'query={}'.format(parsed_path.query),
+            'request_version={}'.format(self.request_version),
+            '',
+            'SERVER VALUES:',
+            'server_version={}'.format(self.server_version),
+            'sys_version={}'.format(self.sys_version),
+            'protocol_version={}'.format(self.protocol_version),
+            '',
+            'HEADERS RECEIVED:',
+        ]
+        for name, value in sorted(self.headers.items()):
+            message_parts.append(
+                '{}={}'.format(name, value.rstrip())
+            )
+        message_parts.append('')
+        message = '\r\n'.join(message_parts)
+        self.send_response(200)
+        self.send_header('Content-Type',
+                         'text/plain; charset=utf-8')
+        self.end_headers()
+        self.wfile.write(message.encode('utf-8'))
+'''
+
+if __name__ == '__main__':
+    from http.server import HTTPServer
+    server = HTTPServer(('localhost', 8080), RCBSRequestHandler)
+    print('Starting server, use <Ctrl-C> to stop')
+    server.serve_forever()
---
+++ public/capture.js
@@ -0,0 +1,232 @@
+(function() {
+    // The width and height of the captured photo. We will set the
+    // width to the value defined here, but the height will be
+    // calculated based on the aspect ratio of the input stream.
+  
+    var width = 320;    // We will scale the photo width to this
+    var height = 0;     // This will be computed based on the input stream
+  
+    // |streaming| indicates whether or not we're currently streaming
+    // video from the camera. Obviously, we start at false.
+  
+    var streaming = false;
+  
+    // The various HTML elements we need to configure or control. These
+    // will be set by the startup() function.
+  
+    var video = null;
+    var canvas = null;
+    var photo = null;
+    var startbutton = null;
+  
+/*
+// Older browsers might not implement mediaDevices at all, so we set an empty object first
+if (navigator.mediaDevices === undefined) {
+  navigator.mediaDevices = {};
+}
+
+// Some browsers partially implement mediaDevices. We can't just assign an object
+// with getUserMedia as it would overwrite existing properties.
+// Here, we will just add the getUserMedia property if it's missing.
+if (navigator.mediaDevices.getUserMedia === undefined) {
+  navigator.mediaDevices.getUserMedia = function(constraints) {
+
+    // First get ahold of the legacy getUserMedia, if present
+    var getUserMedia = navigator.webkitGetUserMedia || navigator.mozGetUserMedia;
+
+    // Some browsers just don't implement it - return a rejected promise with an error
+    // to keep a consistent interface
+    if (!getUserMedia) {
+      return Promise.reject(new Error('getUserMedia is not implemented in this browser'));
+    }
+
+    // Otherwise, wrap the call to the old navigator.getUserMedia with a Promise
+    return new Promise(function(resolve, reject) {
+      getUserMedia.call(navigator, constraints, resolve, reject);
+    });
+  }
+}
+
+navigator.mediaDevices.getUserMedia({ audio: true, video: true })
+.then(function(stream) {
+  var video = document.querySelector('video');
+  // Older browsers may not have srcObject
+  if ("srcObject" in video) {
+    video.srcObject = stream;
+  } else {
+    // Avoid using this in new browsers, as it is going away.
+    video.src = window.URL.createObjectURL(stream);
+  }
+  video.onloadedmetadata = function(e) {
+    video.play();
+  };
+})
+.catch(function(err) {
+  console.log(err.name + ": " + err.message);
+});
+*/
+
+
+    function startup() {
+      video = document.getElementById('video');
+      //var video = document.querySelector('video');
+      canvas = document.getElementById('canvas');
+      photo = document.getElementById('photo');
+      startbutton = document.getElementById('startbutton');
+  
+      // Older browsers might not implement mediaDevices at all, so we set an empty object first
+      if (navigator.mediaDevices === undefined) {
+        navigator.mediaDevices = {};
+      }
+
+      // Some browsers partially implement mediaDevices. We can't just assign an object
+      // with getUserMedia as it would overwrite existing properties.
+      // Here, we will just add the getUserMedia property if it's missing.
+      if (navigator.mediaDevices.getUserMedia === undefined) {
+        navigator.mediaDevices.getUserMedia = function(constraints) {
+
+          // First get ahold of the legacy getUserMedia, if present
+          var getUserMedia = navigator.webkitGetUserMedia || navigator.mozGetUserMedia;
+
+          // Some browsers just don't implement it - return a rejected promise with an error
+          // to keep a consistent interface
+          if (!getUserMedia) {
+            return Promise.reject(new Error('getUserMedia is not implemented in this browser'));
+          }
+
+          // Otherwise, wrap the call to the old navigator.getUserMedia with a Promise
+          return new Promise(function(resolve, reject) {
+            getUserMedia.call(navigator, constraints, resolve, reject);
+          });
+        }
+      }
+
+      navigator.mediaDevices.getUserMedia({ video: true })
+      .then(function(stream) {
+        // Older browsers may not have srcObject
+        if ("srcObject" in video) {
+          video.srcObject = stream;
+        } else {
+          // Avoid using this in new browsers, as it is going away.
+          video.src = window.URL.createObjectURL(stream);
+        }
+        video.onloadedmetadata = function(e) {
+          video.play();
+        };
+
+        video.addEventListener('canplay', function(ev){
+          if (!streaming) {
+            height = video.videoHeight / (video.videoWidth/width);
+          
+            // Firefox currently has a bug where the height can't be read from
+            // the video, so we will make assumptions if this happens.
+          
+            if (isNaN(height)) {
+              height = width / (4/3);
+            }
+          
+            video.setAttribute('width', width);
+            video.setAttribute('height', height);
+            canvas.setAttribute('width', width);
+            canvas.setAttribute('height', height);
+            streaming = true;
+          }
+        }, false);
+
+      })
+      .catch(function(err) {
+        console.log(err.name + ": " + err.message);
+      });
+
+
+
+/*
+      navigator.getMedia = ( navigator.mediaDevices.getUserMedia ||
+                             navigator.getUserMedia ||
+                             navigator.webkitGetUserMedia ||
+                             navigator.mozGetUserMedia ||
+                             navigator.msGetUserMedia);
+  
+      navigator.getMedia(
+        {
+          video: true,
+          audio: false
+        },
+        function(stream) {
+          if (navigator.mozGetUserMedia) {
+            video.mozSrcObject = stream;
+          } else {
+            var vendorURL = window.URL || window.webkitURL;
+            video.src = vendorURL.createObjectURL(stream);
+          }
+          video.play();
+        },
+        function(err) {
+          console.log("getMedia error occured! " + err);
+        }
+      );
+  
+      video.addEventListener('canplay', function(ev){
+        if (!streaming) {
+          height = video.videoHeight / (video.videoWidth/width);
+        
+          // Firefox currently has a bug where the height can't be read from
+          // the video, so we will make assumptions if this happens.
+        
+          if (isNaN(height)) {
+            height = width / (4/3);
+          }
+        
+          video.setAttribute('width', width);
+          video.setAttribute('height', height);
+          canvas.setAttribute('width', width);
+          canvas.setAttribute('height', height);
+          streaming = true;
+        }
+      }, false);
+      */
+  
+      startbutton.addEventListener('click', function(ev){
+        takepicture();
+        ev.preventDefault();
+      }, false);
+      
+      clearphoto();
+    }
+  
+    // Fill the photo with an indication that none has been
+    // captured.
+  
+    function clearphoto() {
+      var context = canvas.getContext('2d');
+      context.fillStyle = "#AAA";
+      context.fillRect(0, 0, canvas.width, canvas.height);
+  
+      var data = canvas.toDataURL('image/png');
+      photo.setAttribute('src', data);
+    }
+    
+    // Capture a photo by fetching the current contents of the video
+    // and drawing it into a canvas, then converting that to a PNG
+    // format data URL. By drawing it on an offscreen canvas and then
+    // drawing that to the screen, we can change its size and/or apply
+    // other changes before drawing it.
+  
+    function takepicture() {
+      var context = canvas.getContext('2d');
+      if (width && height) {
+        canvas.width = width;
+        canvas.height = height;
+        context.drawImage(video, 0, 0, width, height);
+      
+        var data = canvas.toDataURL('image/png');
+        photo.setAttribute('src', data);
+      } else {
+        clearphoto();
+      }
+    }
+  
+    // Set up our event listener to run the startup process
+    // once loading is complete.
+    window.addEventListener('load', startup, false);
+  })();
